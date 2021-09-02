f2cc2f3322ea57e291ef782c21d64077fec7efe9
bojasv <59419054+bojasv@users.noreply.github.com>
2021-09-02 10:00:22 +0530

    Update gen_reports_sys.cmd

--- MS Windows/CMD/gen_reports_sys.cmd
+++ MS Windows/CMD/gen_reports_sys.cmd
@@ -16,6 +16,19 @@ wmic memorychip get > c:\users\bojasva\documents\report_RAM.txt
 :: To get info about any command - Use " /?" as command suffix or "help " as command prefix 
 wmic /? > c:\users\bojasva\documents\help_WMIC.txt
 
+:: to get selected attributes
+wmic memorychip get capacity,name,manufacturer,speed,status > c:\users\bojasva\documents\report_RAM_attr.txt
+
+:: WRKACTJOB - active jobs
+wmic process > c:\users\bojasva\documents\report_processes.txt
+:: wmic process list > c:\users\bojasva\documents\report_processes_list.txt
+:: wmic process list brief > c:\users\bojasva\documents\report_processes_list_brief.txt
+
+:: print all running apps, Programmes, and Their RAM/Memory Usage - wmic process get workingsetsize,commandline
+:: startup apps - wmic startup
+:: services - wmic service list
+:: kill a process - wmic process where name="chrome.exe" call terminate
+
 :: Recently used websites
 ipconfig /displaydns | find "Record Name" > c:\users\bojasva\documents\report_browse.txt
 :: if report_browse.txt is already opened in notepad, it'd need to be closed to get it replaced in before re-running that script???
